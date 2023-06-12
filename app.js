d934f6a697a3e3e0e29e9a1d4b0bf39365a08af9
Mateusz Urbaniak <matteus.urbaniak@hotmail.com>
2023-06-12 18:50:53 +0200

    Basic express application with static file.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const app = express()
 
@@ -8,13 +9,14 @@ const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extends: false}))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found!!</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(3000, () => {
--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,14 @@
 
 const express = require('express')
+const path = require('path')
+
+const rootDir = require('../utils/path')
 
 const router = express.Router()
 
 //all routes are access by /admin/...
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/admin/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+    res.sendFile(path.join(rootDir,'views', 'add-product.html'))
 })
 
 router.post('/product', (req, res, next) => {
