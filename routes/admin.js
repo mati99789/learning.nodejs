636adf77fb596a16901faf86812682a76e53fc8f
Mateusz Urbaniak <matteus.urbaniak@hotmail.com>
2023-06-13 19:38:05 +0200

    view engine pug

--- routes/admin.js
+++ routes/admin.js
@@ -7,13 +7,18 @@ const rootDir = require('../utils/path')
 const router = express.Router()
 
 //all routes are access by /admin/...
+
+const products = []
+
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir,'views', 'add-product.html'))
+    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'})
 })
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
+    products.push({title: req.body.title})
     res.redirect('/')
 })
 
 
-module.exports = router
+exports.routes = router
+exports.products = products
