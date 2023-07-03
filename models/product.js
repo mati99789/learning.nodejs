93b2fc715fc428fab2216e92e7f51d82cabdf97e
Matteus Urbaniak <matteus.urbaniak@hotmail.com>
2023-07-03 19:25:38 +0200

    Add simple creation file json

--- models/product.js
+++ models/product.js
@@ -1,14 +1,33 @@
-const products = []
+const fs = require('fs')
+const path = require('path')
+
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
+const getProductFromFile = (cb) => {
+    fs.readFile(p, (error, fileContent) => {
+        if(error){
+            return cb([])
+        }
+
+        return cb(JSON.parse(fileContent))
+    })
+}
+
 module.exports = class Product {
     constructor(title) {
         this.title = title
     }
 
     save() {
-        products.push(this)
+        getProductFromFile((products) =>{
+            products.push(this)
+
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err)
+            })
+        })
     }
 
-    static fetchAll() {
-        return products
+    static fetchAll(cb) {
+        getProductFromFile(cb)
     }
 }
